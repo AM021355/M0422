1e4b6633ee86ff1488655514cdd7981819e618e5
am021352 <clovercry123@gmail.com>
2015-04-22 00:32:51 +0800

    AM021352

--- all.java
+++ all.java
@@ -7,5 +7,8 @@ public class all
 		System.out.println("CSIE");
 		System.out.println("AM021355");
 		System.out.println("Robert");
+		System.out.println("CSIE");
+		System.out.println("AM021352");
+		System.out.println("Chen4sh");
 	}
 }
