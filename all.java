a44f68e268997504253fcf1985dcdffd6b248ffa
AM021355 <a0912481437@gmail.com>
2015-04-22 00:28:47 +0800

    develope

---
+++ all.java
@@ -0,0 +1,11 @@
+import java.until.Scanner;
+
+public class all
+{
+	public static void main(String[] args)
+	{
+		System.out.println("CSIE");
+		System.out.println("AM021355");
+		System.out.println("Robert");
+	}
+}
